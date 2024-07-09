23c6340af355cecc6d431da07644c9a79380fcb5
chandraSekhar-alti <althichandu123@gmail.com>
2024-07-09 12:02:20 +0530

    initial commit in api testsing

---
+++ src/test/java/API/BaseTest.java
@@ -0,0 +1,29 @@
+package API;
+
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    Playwright playwright;
+    APIRequest apiRequest;
+    APIRequestContext apiRequestContext;
+
+    @BeforeMethod
+    public void setup() {
+        playwright = Playwright.create();
+        apiRequest = playwright.request();
+        apiRequestContext = apiRequest.newContext();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        playwright.close();
+    }
+
+    public APIRequestContext getApiRequestContext(){
+        return apiRequestContext;
+    }
+}
---
+++ src/test/java/API/GETAPICallTest.java
@@ -0,0 +1,66 @@
+package API;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.options.RequestOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class GETAPICallTest extends BaseTest{
+
+
+    @Test
+    public void getSpecificUserApiTest() throws IOException {
+        APIRequestContext apiRequestContext = getApiRequestContext();
+        APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users",
+
+                RequestOptions.create()
+                        .setQueryParam("id", "7018213")
+                        .setQueryParam("status", "active")
+
+        );
+        Assert.assertTrue(apiResponse.ok());
+        Assert.assertEquals(apiResponse.status(), 200);
+        Assert.assertEquals(apiResponse.statusText(), "OK");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode response = objectMapper.readTree(apiResponse.body());
+//        String jsonPrettyResponse = response.toPrettyString();
+
+        Assert.assertTrue(response.isArray(), "Response is not an array");
+        Assert.assertEquals(response.size(), 1, "Array size is not 1");
+
+        JsonNode user = response.get(0);
+        String name = user.get("name").asText();
+        String email = user.get("email").asText();
+        Assert.assertEquals(name, "Kamla Marar");
+        Assert.assertEquals(email, "marar_kamla@kris.example");
+    }
+
+    @Test
+    public void getUserApiTest() throws IOException {
+        APIRequestContext apiRequestContext = getApiRequestContext();
+
+        APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users");
+
+        int statusCode = apiResponse.status();
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertTrue(apiResponse.ok());
+        String statusResText = apiResponse.statusText();
+        Assert.assertEquals(statusResText, "OK");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse = objectMapper.readTree(apiResponse.body());
+        String jsonPrettyResponse = jsonResponse.toPrettyString();
+
+        Map<String, String> headersMap = apiResponse.headers();
+        Assert.assertEquals(headersMap.get("connection"), "close");
+        Assert.assertEquals(headersMap.get("x-download-options"), "noopen");
+
+    }
+}
